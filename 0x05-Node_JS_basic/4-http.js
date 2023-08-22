1ff90211264c3181cb1fcd5301a77dfda0e853bb
Sxamoecode <samuelayandare@gmail.com>
2023-08-22 17:32:53 +0100

    Nodejs

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostName = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((request, response) => {
@@ -9,7 +9,7 @@ const app = http.createServer((request, response) => {
   response.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
+app.listen(port, hostName, () => {
 });
 
 module.exports = app;
